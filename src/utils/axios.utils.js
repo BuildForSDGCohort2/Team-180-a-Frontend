f765d1baa9d432be312107130621efb3ea119536
Collins Muriuki <murerwacollins@gmail.com>
2020-08-29 16:18:28 +0300

    Update auth utils and custom axios instance

--- src/utils/axios.utils.js
+++ src/utils/axios.utils.js
@@ -3,11 +3,13 @@ import axios from 'axios'
 import { getJwtToken } from './auth.utils'
 import config from '../config'
 
+const jwtToken = getJwtToken()
+
 // axios instance to be used to make http requests from the client
 const axiosInstance = axios.create({
   baseURL: config.serverHost,
   headers: {
-    Authorization: `Bearer ${getJwtToken()}`
+    Authorization: jwtToken != null ? `Bearer ${jwtToken}` : null
   }
 })
 
