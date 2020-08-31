457dc97ab9a5e9f8b662b9c3de6c97b8326b15f6
Collins Muriuki <murerwacollins@gmail.com>
2020-08-31 14:08:31 +0300

    feat: Install and configure Ant Design and add ability to cutomize theme

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'antd'
 
 const HomePage = () => {
-  return <div>Hello from HomePage</div>
+  return (
+    <div>
+      Hello from HomePages
+      <Button type='primary'>Button</Button>
+    </div>
+  )
 }
 
 export default HomePage
